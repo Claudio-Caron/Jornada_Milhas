3a0136c2b2e398cf9140bd99bfa2f6a346ac61d8
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2025-04-15 16:19:14 -0400

    adicionar container mySql no projeto para persistir de maneira mais prática. configurar Setup e Teardown async por meio da interface IAsyncLifeTime e aplicar migrations na budança do banco

--- JornadaMilhas.Test.Integration/ContextoFixture.cs
+++ JornadaMilhas.Test.Integration/ContextoFixture.cs
@@ -6,23 +6,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Testcontainers.MsSql;
 
 namespace JornadaMilhas.Test.Integration;
 
-public class ContextoFixture
+public class ContextoFixture:IAsyncLifetime
 {
-    public  JornadaMilhasContext Context {  get; }
+    public JornadaMilhasContext Context { get; private set; }
+    
+    private readonly MsSqlContainer _msSqlContainer= new MsSqlBuilder
+        ()
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+        .Build();
 
     public ContextoFixture()
     {
 
-        var options = new DbContextOptionsBuilder<JornadaMilhasContext>()
-            .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=JornadaMilhas;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False")
-            .Options;
-        Context = new JornadaMilhasContext(options);
+        
         
     }
 
+    public async Task InitializeAsync()
+    {
+        await _msSqlContainer.StartAsync();
+        var options = new DbContextOptionsBuilder<JornadaMilhasContext>()
+            .UseSqlServer(_msSqlContainer.GetConnectionString())
+            .Options;
+        Context = new JornadaMilhasContext(options);
+        Context.Database.Migrate();
+    }
 
+    public async Task DisposeAsync()
+    {
+        await _msSqlContainer.StopAsync();
+    }
 }
 
